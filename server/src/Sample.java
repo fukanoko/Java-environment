8618e5fc4c4d34d192486da05d3cc055474b2d05
fukanoko <fukanoko0312@gmail.com>
2020-09-08 22:09:15 +0900

    Java environment

---
+++ server/src/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 10;
+        a = a + b;
+        System.out.println(a);
+    }
+}
